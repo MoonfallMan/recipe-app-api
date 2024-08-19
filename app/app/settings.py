c87dd53942c27b946540cb82ab5d5a99359e5f86
Cory Moonfall <moonfallcory@gmail.com>
2024-08-19 07:20:59 -0700

    keepup

--- app/app/settings.py
+++ app/app/settings.py
@@ -26,6 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework.authtoken',
     'core',
     'rest_framework',
     'drf_spectacular',
@@ -68,7 +70,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'HOST' : os.environ.get('DB_HOST'),
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
